c07db9972a17441811b0ff93b564b439c26313c4
Randall Song <randallsong149@hotmail.com>
2017-08-01 17:44:20 -0400

    x's and o's appear

--- script.js
+++ script.js
@@ -1,111 +1,158 @@
 //tic tac toe javacript
 //function that takes into account whose turn it is and changes
 //the square accordingly
-	var turn = 0;
 
-function tictactoe(){
+var turn = 0;
+
+var box1 = "undefined";
+
+var status = [box1, "undefined", "undefined",
+			  "undefined", "undefined", "undefined", 
+			  "undefined", "undefined", "undefined"];
+
+function tictactoe(){}
+
+
+
+tictactoe.prototype.clickBox1 = function(){
+		if(turn == 0){
+			document.getElementById('x1').style.display = "block";
+			turn++;
+			status[0] = "x";
+		}else if(turn == 1){
+			document.getElementById('o1').style.display = "block";
+			turn--;
+			status[0] = "o";
+		}
+}
+
+tictactoe.prototype.clickBox2 = function(){
+		if(turn == 0){
+			document.getElementById('x2').style.display = "block";
+			turn++;
+		}else if(turn == 1){
+			document.getElementById('o2').style.display = "block";
+			turn--;
+		}
+}
+
+tictactoe.prototype.clickBox3 = function(){
+		if(turn == 0){
+			document.getElementById('x3').style.display = "block";
+			turn++;
+		}else if(turn == 1){
+			document.getElementById('o3').style.display = "block";
+			turn--;
+		}
+}
+
+tictactoe.prototype.clickBox4 = function(){
+		if(turn == 0){
+			document.getElementById('x4').style.display = "block";
+			turn++;
+		}else if(turn == 1){
+			document.getElementById('o4').style.display = "block";
+			turn--;
+		}
+}
+
+tictactoe.prototype.clickBox5 = function(){
+		if(turn == 0){
+			document.getElementById('x5').style.display = "block";
+			turn++;
+		}else if(turn == 1){
+			document.getElementById('o5').style.display = "block";
+			turn--;
+		}
+}
+
+tictactoe.prototype.clickBox6 = function(){
+		if(turn == 0){
+			document.getElementById('x6').style.display = "block";
+			turn++;
+		}else if(turn == 1){
+			document.getElementById('o6').style.display = "block";
+			turn--;
+		}
+}
+
+tictactoe.prototype.clickBox7 = function(){
+		if(turn == 0){
+			document.getElementById('x7').style.display = "block";
+			turn++;
+		}else if(turn == 1){
+			document.getElementById('o7').style.display = "block";
+			turn--;
+		}
+}
+
+tictactoe.prototype.clickBox8 = function(){
+		if(turn == 0){
+			document.getElementById('x8').style.display = "block";
+			turn++;
+		}else if(turn == 1){
+			document.getElementById('o8').style.display = "block";
+			turn--;
+		}
+}
+
+tictactoe.prototype.clickBox9 = function(){
+		if(turn == 0){
+			document.getElementById('x9').style.display = "block";
+			turn++;
+		}else if(turn == 1){
+			document.getElementById('o9').style.display = "block";
+			turn--;
+		}
+}
+
+tictactoe.prototype.checkWinner = function(){
 
 }
 
-tictactoe.prototype.clickX = function(){
-	if(turn = 0){
-	document.getElementById('x1').style.display = "block";
-	turn++;
-	}
-	else{
-		alert("you can't do that")
-	}
-}
+var newGame = new tictactoe()
 
-tictactoe.prototype.clickO = function(){
-	if (turn = 1){
-	document.getElementById('o1').style.display = "block";
-	turn--;
-	}
-}
-
-// tictactoe.prototype.clickX2 = function(){
-// 	document.getElementById('box2') = document.getElementById('box2').style.display = block;
-// 	turn++;
-// }
-
-// tictactoe.prototype.clickX3 = function(){
-// 	document.getElementById('box3') = document.getElementById('box3').style.display = block;
-// 	turn++;
-// }
-
-// tictactoe.prototype.clickX4 = function(){
-// 	document.getElementById('box4') = document.getElementById('box4').style.display = block;
-// 	turn++;
-// }
-
-// tictactoe.prototype.clickX5 = function(){
-// 	document.getElementById('box5') = document.getElementById('box5').style.display = block;
-// 	turn++;
-// }
-
-// tictactoe.prototype.clickX6 = function(){
-// 	document.getElementById('box6') = document.getElementById('box6').style.display = block;
-// 	turn++;
-// }
-
-// tictactoe.prototype.clickX7 = function(){
-// 	document.getElementById('box7') = document.getElementById('box7').style.display = block;
-// 	turn++;
-// }
-
-// tictactoe.prototype.clickX8 = function(){
-// 	document.getElementById('box8') = document.getElementById('box8').style.display = block;
-// 	turn++;
-// }
-
-// tictactoe.prototype.clickX9 = function(){
-// 	document.getElementById('box9') = document.getElementById('box9').style.display = block;
-// 	turn++;
-// }
-
-
-newGame = new tictactoe()
 
 document.getElementById('box1').onclick = function(){
-	newGame.clickX();
+	newGame.clickBox1();
+	if(turn == 0){
+		status[0] = "x";
+	}else if(turn == 1){
+		status[0] = "o";
+	}
 }
 
-// document.getElementById('box2').onclick = function(){
-// 	newGame.clickX2();
-// }
+document.getElementById('box2').onclick = function(){
+	newGame.clickBox2();
+}
 
-// document.getElementById('box3').onclick = function(){
-// 	newGame.clickX3();
-// }
+document.getElementById('box3').onclick = function(){
+	newGame.clickBox3();
+}
 
-// document.getElementById('box4').onclick = function(){
-// 	newGame.clickX4();
-// }
+document.getElementById('box4').onclick = function(){
+	newGame.clickBox4();
+}
 
-// document.getElementById('box5').onclick = function(){
-// 	newGame.clickX5();
-// }
+document.getElementById('box5').onclick = function(){
+	newGame.clickBox5();
+}
 
-// document.getElementById('box6').onclick = function(){
-// 	newGame.clickX6();
-// }
+document.getElementById('box6').onclick = function(){
+	newGame.clickBox6();
+}
 
-// document.getElementById('box7').onclick = function(){
-// 	newGame.clickX7();
-// }
+document.getElementById('box7').onclick = function(){
+	newGame.clickBox7();
+}
 
-// document.getElementById('box8').onclick = function(){
-// 	newGame.clickX8();
-// }
+document.getElementById('box8').onclick = function(){
+	newGame.clickBox8();
+}
 
-// document.getElementById('box9').onclick = function(){
-// 	newGame.clickX9();
-// }
-
-//a function that check if there is a row of three anywhere and
-//ends the game when it finds one
-
+document.getElementById('box9').onclick = function(){
+	newGame.clickBox9();
+}
 //maybe give each square a status of blank, X, or O, then check the
 //possibilities
 
