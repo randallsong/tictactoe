e3e8f3b91ef292055055ee14d02432ef338847ec
Randall Song <randallsong149@hotmail.com>
2017-08-02 10:01:01 -0400

    can detect winner but does not stop

--- script.js
+++ script.js
@@ -5,10 +5,16 @@
 var turn = 0;
 
 var box1 = "undefined";
+var box2 = "undefined";
+var box3 = "undefined";
+var box4 = "undefined";
+var box5 = "undefined";
+var box6 = "undefined";
+var box7 = "undefined";
+var box8 = "undefined";
+var box9 = "undefined";
 
-var status = [box1, "undefined", "undefined",
-			  "undefined", "undefined", "undefined", 
-			  "undefined", "undefined", "undefined"];
+//fuck arrays they don't work
 
 function tictactoe(){}
 
@@ -18,11 +24,11 @@ tictactoe.prototype.clickBox1 = function(){
 		if(turn == 0){
 			document.getElementById('x1').style.display = "block";
 			turn++;
-			status[0] = "x";
+			box1 = "x";
 		}else if(turn == 1){
 			document.getElementById('o1').style.display = "block";
 			turn--;
-			status[0] = "o";
+			box1 = "o";
 		}
 }
 
@@ -30,9 +36,11 @@ tictactoe.prototype.clickBox2 = function(){
 		if(turn == 0){
 			document.getElementById('x2').style.display = "block";
 			turn++;
+			box2 = "x";
 		}else if(turn == 1){
 			document.getElementById('o2').style.display = "block";
 			turn--;
+			box2 = "o";
 		}
 }
 
@@ -40,9 +48,11 @@ tictactoe.prototype.clickBox3 = function(){
 		if(turn == 0){
 			document.getElementById('x3').style.display = "block";
 			turn++;
+			box3 = "x";
 		}else if(turn == 1){
 			document.getElementById('o3').style.display = "block";
 			turn--;
+			box3 = "o";
 		}
 }
 
@@ -50,9 +60,11 @@ tictactoe.prototype.clickBox4 = function(){
 		if(turn == 0){
 			document.getElementById('x4').style.display = "block";
 			turn++;
+			box4 = "x";
 		}else if(turn == 1){
 			document.getElementById('o4').style.display = "block";
 			turn--;
+			box4 = "o";
 		}
 }
 
@@ -60,9 +72,11 @@ tictactoe.prototype.clickBox5 = function(){
 		if(turn == 0){
 			document.getElementById('x5').style.display = "block";
 			turn++;
+			box5 = "x";
 		}else if(turn == 1){
 			document.getElementById('o5').style.display = "block";
 			turn--;
+			box5 = "o";
 		}
 }
 
@@ -70,9 +84,11 @@ tictactoe.prototype.clickBox6 = function(){
 		if(turn == 0){
 			document.getElementById('x6').style.display = "block";
 			turn++;
+			box6 = "x";
 		}else if(turn == 1){
 			document.getElementById('o6').style.display = "block";
 			turn--;
+			box6 = "o";
 		}
 }
 
@@ -80,9 +96,11 @@ tictactoe.prototype.clickBox7 = function(){
 		if(turn == 0){
 			document.getElementById('x7').style.display = "block";
 			turn++;
+			box7 = "x";
 		}else if(turn == 1){
 			document.getElementById('o7').style.display = "block";
 			turn--;
+			box7 = "o";
 		}
 }
 
@@ -90,9 +108,11 @@ tictactoe.prototype.clickBox8 = function(){
 		if(turn == 0){
 			document.getElementById('x8').style.display = "block";
 			turn++;
+			box8 = "x";
 		}else if(turn == 1){
 			document.getElementById('o8').style.display = "block";
 			turn--;
+			box8 = "o";
 		}
 }
 
@@ -100,14 +120,53 @@ tictactoe.prototype.clickBox9 = function(){
 		if(turn == 0){
 			document.getElementById('x9').style.display = "block";
 			turn++;
+			box9 = "x";
 		}else if(turn == 1){
 			document.getElementById('o9').style.display = "block";
 			turn--;
+			box9 = "o";
 		}
 }
+//functions that check every box depending on whose turn it is an fills it with an
+//X or O accordingly
+
+
 
 tictactoe.prototype.checkWinner = function(){
-
+	if(box1 == "x" && box2 == "x" && box3 == "x"){
+		alert("player 1 is the winner!")
+	}else if(box4 == "x" && box5 == "x" && box6 == "x"){
+		alert("player 1 is the winner!")
+	}else if(box7 == "x" && box8 == "x" && box9 == "x"){
+		alert("player 1 is the winner!")
+	}else if(box1 == "x" && box4 == "x" && box7 == "x"){
+		alert("player 1 is the winner!")
+	}else if(box2 == "x" && box5 == "x" && box8 == "x"){
+		alert("player 1 is the winner!")
+	}else if(box3 == "x" && box6 == "x" && box9 == "x"){
+		alert("player 1 is the winner!")
+	}else if(box1 == "x" && box5 == "x" && box9 == "x"){
+		alert("player 1 is the winner!")
+	}else if(box3 == "x" && box5 == "x" && box7 == "x"){
+		alert("player 1 is the winner!")
+	}
+	if(box1 == "o" && box2 == "o" && box3 == "o"){
+		alert("player 1 is the winner!")
+	}else if(box4 == "o" && box5 == "o" && box6 == "x"){
+		alert("player 1 is the winner!")
+	}else if(box7 == "o" && box8 == "o" && box9 == "o"){
+		alert("player 1 is the winner!")
+	}else if(box1 == "o" && box4 == "o" && box7 == "o"){
+		alert("player 1 is the winner!")
+	}else if(box2 == "o" && box5 == "o" && box8 == "o"){
+		alert("player 1 is the winner!")
+	}else if(box3 == "o" && box6 == "o" && box9 == "o"){
+		alert("player 1 is the winner!")
+	}else if(box1 == "o" && box5 == "o" && box9 == "o"){
+		alert("player 1 is the winner!")
+	}else if(box3 == "o" && box5 == "o" && box7 == "o"){
+		alert("player 1 is the winner!")
+	}
 }
 
 var newGame = new tictactoe()
@@ -115,43 +174,47 @@ var newGame = new tictactoe()
 
 document.getElementById('box1').onclick = function(){
 	newGame.clickBox1();
-	if(turn == 0){
-		status[0] = "x";
-	}else if(turn == 1){
-		status[0] = "o";
-	}
+	newGame.checkWinner();
 }
 
 document.getElementById('box2').onclick = function(){
 	newGame.clickBox2();
+	newGame.checkWinner();
 }
 
 document.getElementById('box3').onclick = function(){
 	newGame.clickBox3();
+	newGame.checkWinner();
 }
 
 document.getElementById('box4').onclick = function(){
 	newGame.clickBox4();
+	newGame.checkWinner();
 }
 
 document.getElementById('box5').onclick = function(){
 	newGame.clickBox5();
+	newGame.checkWinner();
 }
 
 document.getElementById('box6').onclick = function(){
 	newGame.clickBox6();
+	newGame.checkWinner();
 }
 
 document.getElementById('box7').onclick = function(){
 	newGame.clickBox7();
+	newGame.checkWinner();
 }
 
 document.getElementById('box8').onclick = function(){
 	newGame.clickBox8();
+	newGame.checkWinner();
 }
 
 document.getElementById('box9').onclick = function(){
 	newGame.clickBox9();
+	newGame.checkWinner();
 }
 //maybe give each square a status of blank, X, or O, then check the
 //possibilities
